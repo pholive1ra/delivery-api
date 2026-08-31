e554da977bf87f6262611fcb89456339a39eb7b2
Pedro Oliveira <pedro597381@gmail.com>
2026-08-31 00:17:56 -0300

    Configuring SecurityConfig in 'config'

---
+++ src/main/java/com/pedro/delivery_api/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.pedro.delivery_api.config;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configure(http))
+
+    } }
