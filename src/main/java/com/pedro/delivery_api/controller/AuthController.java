0a0adbdf00bbeab68b87adaed91a0c0ff6573fae
Pedro <pedrohenrqdev@gmail.com>
2026-09-04 16:42:33 -0300

    Implementing AuthenticationManager in Auth

--- src/main/java/com/pedro/delivery_api/controller/AuthController.java
+++ src/main/java/com/pedro/delivery_api/controller/AuthController.java
@@ -7,35 +7,46 @@ import com.pedro.delivery_api.dto.response.LoginResponseDTO;
 import com.pedro.delivery_api.dto.response.RegisterUserResponseDTO;
 import com.pedro.delivery_api.entity.Role;
 import com.pedro.delivery_api.entity.User;
+import com.pedro.delivery_api.exception.ResourceNotFoundException;
 import com.pedro.delivery_api.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
-        return null;
+
+
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponseDTO> register(@Valid @RequestBody RegisterUserRequestDTO request) {
         User newUser = new User();
         newUser.setEmail(request.email());
-        newUser.setPassword(request.password()); //Senha terá que ser hash nao salva pura
+        newUser.setPassword(passwordEncoder.encode(request.password())); //Senha terá que ser hash nao salva pura
         newUser.setRole(Role.CUSTOMER);
         newUser.setName(request.name());
 
