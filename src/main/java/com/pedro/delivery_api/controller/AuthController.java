a78b0cff38c372eaefba430df3352f5431b023f4
Pedro Oliveira <pedro597381@gmail.com>
2026-09-06 03:16:54 -0300

    Implementing JWT Generation with TokenConfig class and configuring AuthController, AuthConfig...

--- src/main/java/com/pedro/delivery_api/controller/AuthController.java
+++ src/main/java/com/pedro/delivery_api/controller/AuthController.java
@@ -13,6 +13,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,7 +40,8 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
-
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
 
     }
 
