78aac021f48efdb4a2155c6feabacf4c5f9cdf0f
Pedro <pedrohenrqdev@gmail.com>
2026-09-14 17:37:06 -0300

    feat: implement JWT authentication filter

--- src/main/java/com/pedro/delivery_api/config/TokenConfig.java
+++ src/main/java/com/pedro/delivery_api/config/TokenConfig.java
@@ -3,6 +3,7 @@ package com.pedro.delivery_api.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.pedro.delivery_api.entity.User;
+import com.pedro.delivery_api.security.JwtAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -13,7 +14,6 @@ public class TokenConfig {
     private String secret = "secret";
 
     public String generateToken(User user) {
-
     Algorithm algorithm = Algorithm.HMAC256(secret);
     return JWT.create()
             .withClaim("userId", user.getId())
@@ -21,6 +21,13 @@ public class TokenConfig {
             .withExpiresAt(Instant.now().plusSeconds(86400))
             .withIssuedAt(Instant.now())
             .sign(algorithm);
+    }
 
+    public String validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm) //Verificar o token seguindo as regras abaixo:
+                .build()
+                .verify(token)
+                .getSubject();
     }
 }
